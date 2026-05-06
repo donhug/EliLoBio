34b1e2f82018d4323f794343858884c6831f1bdd
hugo ferey <ferey.hugo@gmail.com>
2026-05-07 00:08:09 +0200

    feat: mise en place de react Router

--- EliLoBio/src/App.jsx
+++ EliLoBio/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Routes, Route, Router} from "react-router-dom";
 import './App.css'
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Accueil from "./pages/accueil/Accueil.jsx"
+import Produits from "./pages/produits/Produits.jsx"
+import Erreur from "./pages/erreur/Erreur.jsx"
 
 function App() {
   return (
-      <h1>EliLoBio</h1>
+      <Header/>
   )
 }
-
 export default App
