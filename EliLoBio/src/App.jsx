20d67da84648c9ba3f805d706fd84d780dfb989d
hugo ferey <ferey.hugo@gmail.com>
2026-05-06 22:41:16 +0200

    chore: mise en place du projet

---
+++ EliLoBio/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.css'
+
+function App() {
+  return (
+      <h1>EliLoBio</h1>
+  )
+}
+
+export default App
---
+++ EliLoBio/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
